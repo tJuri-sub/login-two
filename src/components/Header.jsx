3c03affdadfeb376d1d6b7f21f01a63071f175cc
juri turiano <juricuber@gmail.com>
2024-06-24 21:19:26 +0800

    header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,8 @@
 export const Header = () => {
   return (
-    <div className="w-full flex items-center">
+    <div className="w-full flex items-center p-2">
       <div className="flex gap-1 items-center   ">
-        <img className="w-[7%]" src="/icon.png" alt="Vigiwheel Icon" />
+        <img className="w-[4%]" src="/icon.png" alt="Vigiwheel Icon" />
         <h1 className="text-white text-xl font-bold">VigiWheel</h1>
       </div>
     </div>
